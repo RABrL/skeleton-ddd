45be98edf5f57eb07fa4b828d71b5dc3b9dad2a0
Robinson Brito <robin_brito@hotmail.com>
2026-03-05 15:30:58 -0500

    Add command and query bus infrastructure with associated domain classes
    - Introduced Command and Query abstract classes for command/query handling.
    - Implemented CommandBus and QueryBus interfaces for dispatching commands and asking queries.
    - Created CommandHandler and QueryHandler interfaces for handling respective commands and queries.
    - Added error handling with CommandNotRegisteredError and QueryNotRegisteredError classes.
    - Developed InMemoryCommandBus and InMemoryQueryBus for in-memory command and query processing.
    - Established CommandHandlers and QueryHandlers for managing command and query handlers.

---
+++ packages/core/src/shared/infrastructure/command-bus/CommandHandlers.ts
@@ -0,0 +1,23 @@
+import { Command } from "../../domain/Command";
+import type { CommandHandler } from "../../domain/CommandHandler";
+import { CommandNotRegisteredError } from "../../domain/CommandNotRegisteredError";
+
+export class CommandHandlers extends Map<Command, CommandHandler<Command>> {
+  constructor(commandHandlers: Array<CommandHandler<Command>>) {
+    super();
+
+    commandHandlers.forEach((commandHandler) => {
+      this.set(commandHandler.subscribedTo(), commandHandler);
+    });
+  }
+
+  public override get(command: Command): CommandHandler<Command> {
+    const commandHandler = super.get(command.constructor);
+
+    if (!commandHandler) {
+      throw new CommandNotRegisteredError(command);
+    }
+
+    return commandHandler;
+  }
+}
